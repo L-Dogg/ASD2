7239ffdf2670abdce0372d4d25fa8d2d19a86e4e
L-Dogg <szymek94sul@gmail.com>
2016-03-02 22:47:10 +0100

    cashier problem - dynamic programming

---
+++ Stare/Lab 2 - dynamic Cormen/Lab 2 - dynamic Cormen/Cashier.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2___dynamic_Cormen
+{
+	class Cashier
+	{
+		//dobrane nominaly uniemozliwiaja znalezienie
+		//optymalnego rozwiazania algorytmem zachlannym
+		private int[] nominals = { 1, 5, 6 };
+		
+		//zwraca ile monet uzyjemy do wydania change szekli
+		public int getChange(int change)
+		{
+			int[] tab = new int[change + 1];
+			List<int>[] coins = new List<int>[change + 1];
+						
+			for(int i = 1; i <= change; i++)
+				tab[i] = int.MaxValue;
+
+			return countChange(tab, change);
+		}
+
+		private int countChange(int[] tab, int change)
+		{
+			int q;
+			// juz obliczylismy te wartosc:
+			if (tab[change] < int.MaxValue)
+				return tab[change];
+
+			//wystarczy jedna moneta:
+			for (int i = 0; i < nominals.Length; i++)
+			{
+				if (change == nominals[i])
+				{
+					tab[change] = 1;
+					return 1;
+				}
+			}
+			
+			//rekurencyjnie tak jak Pan Kotowski powiedzial
+			q = int.MaxValue;
+			for(int i = 1; i < change; i++)
+			{
+				q = Math.Min(q, countChange(tab, i) + countChange(tab,change - i));
+			}
+			tab[change] = q;
+			return q;
+		}
+	}
+}
---
+++ Stare/Lab 2 - dynamic Cormen/Lab 2 - dynamic Cormen/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2___dynamic_Cormen
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Cashier cr = new Cashier();
+			Console.WriteLine(cr.getChange(10));
+		}
+	}
+}
