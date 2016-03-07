24200252c6e913c33ca8da2d4ee168d9132b6bda
L-Dogg <szymek94sul@gmail.com>
2016-03-07 21:45:52 +0100

    3 new/old tasks

---
+++ Stare/Lab1_1/Lab1/PriorityQueue.cs
@@ -0,0 +1,272 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ASD
+{
+
+interface IPriorityQueue
+    {
+    void Put(int p);     // wstawia element do kolejki
+    int GetMax();        // pobiera maksymalny element z kolejki (element jest usuwany z kolejki)
+    int ShowMax();       // pokazuje maksymalny element kolejki (element pozostaje w kolejce)
+    int Count { get; }   // liczba elementów kolejki
+    }
+
+
+class LazyPriorityQueue : IPriorityQueue
+    {
+		LinkedList<int> list;
+    public LazyPriorityQueue()
+        {
+			list = new LinkedList<int>();
+        }
+
+    public void Put(int p)
+        {
+			list.AddFirst(p);
+        }
+
+    public int GetMax()
+        {
+			if (list.Count == 0)
+				throw new InvalidOperationException("Access to empty queue");
+            int max = ShowMax();
+            list.Remove(max);
+
+			return max;
+        }
+
+    public int ShowMax()
+        {
+			if (list.Count == 0)
+				throw new InvalidOperationException("Access to empty queue");
+
+			int max = list.First.Value;
+
+			foreach (int el in list)
+				if (el > max)
+					max = el;
+
+			return max;
+		}
+
+    public int Count
+        {
+        get {
+            return list.Count;
+            }
+        }
+
+    } // LazyPriorityQueue
+
+
+class EagerPriorityQueue : IPriorityQueue
+    {
+		LinkedList<int> list;
+		public EagerPriorityQueue()
+        {
+			list = new LinkedList<int>();
+        }
+
+    public void Put(int p)
+        {
+			if (list.Count == 0)
+			{
+				list.AddFirst(p);
+				return;
+			}
+			foreach(int el in list)
+			{
+				if (el <= p)
+				{
+					list.AddBefore(list.Find(el), p);
+					return;
+				}
+			}
+			list.AddLast(p);
+        }
+
+    public int GetMax()
+        {
+			if (list.Count == 0)
+				throw new InvalidOperationException("Access to empty queue");
+			int max = list.First.Value;
+			list.RemoveFirst();
+			return max;
+        }
+
+    public int ShowMax()
+		{
+			if (list.Count == 0)
+				throw new InvalidOperationException("Access to empty queue");
+			return list.First.Value;
+		}
+
+    public int Count
+        {
+        get {
+            return list.Count;
+            }
+        }
+
+    } // EagerPriorityQueue
+
+
+class HeapPriorityQueue : IPriorityQueue
+    {
+		private const int SIZE = 101;
+		private	int n = 0; //indeks ostatniego elementu
+		private int[] tab = new int[SIZE];
+    public HeapPriorityQueue()
+        {
+			tab[0] = int.MaxValue; //wartownik
+        }
+
+    public void Put(int p)
+        {
+			tab[++n] = p;
+			UpHeap(n);
+        }
+
+    public int GetMax()
+        {
+			if (n == 0)
+				throw new InvalidOperationException("Access to empty queue");
+			if (n == 1)
+				return tab[n--];
+
+			int m = tab[1];
+			tab[1] = tab[n--];
+			DownHeap(1);
+
+			return m;
+		}
+
+    public int ShowMax()
+        {
+			if (n == 0)
+				throw new InvalidOperationException("Access to empty queue");
+
+			return tab[1];
+		}
+
+	private void DownHeap(int num)
+		{
+			int k = 2 * num;
+            int v = tab[num];
+			while(k <= n)
+			{
+				if (k + 1 <= n)
+					if (tab[k + 1] > tab[k])
+						k = k + 1;
+				if (tab[k] > v)
+				{
+					tab[num] = tab[k];
+					num = k;
+					k = 2 * num;
+				}
+				else
+					break;
+			}
+			tab[num] = v;
+		}
+
+	private void UpHeap(int num)
+		{
+			int v = tab[num];
+			while(tab[num/2] < v)
+			{
+				tab[num] = tab[num / 2];
+				num = num / 2;
+			}
+			tab[num] = v;
+		
+		}
+    public int Count
+        {
+        get {
+            return 0;
+            }
+        }
+
+    } // HeapPriorityQueue
+
+	class SkewHeap : IPriorityQueue
+	{
+		private int count = 0;
+		private class Node
+		{
+			public int Val { get; set; }
+			public int Npl { get; set; }
+			public Node left, right;
+		};
+		private Node root = null;
+
+		public int Count
+		{
+			get
+			{
+				return count;
+			}
+		}
+
+		public int GetMax()
+		{
+			if(root == null)
+				throw new InvalidOperationException("Access to empty queue");
+			
+			int v = root.Val;
+			root = Union(root.left, root.right);
+			return v;
+		}
+
+		public  void Put(int p)
+		{
+			Node elem = new Node();
+			elem.left = elem.right = null;
+			elem.Val = p;
+			elem.Npl = 0;
+			root = Union(root, elem);
+		}
+
+		public int ShowMax()
+		{
+			if (root == null)
+				throw new InvalidOperationException("Access to empty queue");
+
+			return root.Val;
+		}
+		private Node Union(Node left, Node right)
+		{
+			if (left == null)
+				return right;
+			else if (right == null)
+				return left;
+			Node ret;
+			if (left.Val > right.Val)
+			{
+				ret = left;
+				ret.right = Union(ret.right, right);
+			}
+			else
+			{
+				ret = right;
+				ret.right = Union(ret.right, left);
+			}
+			if (ret.left == null || ret.right.Npl > ret.left.Npl)
+			{
+				Node tmp = ret.right;
+				ret.right = ret.left;
+				ret.left = tmp;
+			}
+			if (ret.right == null)
+				ret.Npl = 0;
+			else
+				ret.Npl = ret.right.Npl + 1;
+
+			return ret;
+		}
+	}
+}
---
+++ Stare/Lab1_2/Lab1_2/hash.cs
@@ -0,0 +1,205 @@
+﻿using System;
+
+namespace ASD
+{
+
+    class HashTable
+    {
+        // klucze w tablicy sa unikalne i nieujemne
+
+        private const int empty = -1;
+        private const int deleted = -2;
+
+        private double alfa; // wspolczynnik wypelnienia przy ktorym tablica jest dwukrotnie powiekszana
+        private int elemCount;
+        private int accessCount;
+        private int[] table;
+        private Func<int, int, int> hash;   // podstawowa funkcja haszujaca
+        private Func<int, int, int> shift;  // funkcja przesuniecia wywolywana w razie konfliktu, drugi parametr - numer iteracji
+
+        // domyslny rozmiar poczatkowy 8
+        // domyslny wspolczynnik zapelnienia powodujacy podwojenie rozmiaru 7/8
+        public HashTable(Func<int, int, int> _hash, Func<int, int, int> _shift, int _size = 8, double _alfa = 7.0/8.0)
+        {
+            hash = _hash;
+            shift = _shift;
+            alfa = _alfa;
+            elemCount = accessCount = 0;
+            table = new int[_size];
+            for (int i = 0; i < _size; i++)
+                table[i] = empty;
+        }
+
+        public int Size
+        {
+            get
+            {
+                return table.Length;
+            }
+        }
+
+        public int AccessCount
+        {
+            get
+            {
+                return accessCount;
+            }
+        }
+
+        public int ElemCount  // liczba elementow w tablicy (z uwzglednieniem skasowanych)
+        {
+            get
+            {
+                return elemCount;
+            }
+        }
+
+        // wyszukiwanie w tablicy elementu v - zwraca true jesli element v jest w tablicy
+        public bool Search(int v)
+		{
+			//Console.WriteLine("Search");
+
+			int i = hash(v, table.Length);
+			int k = 1;
+			while (table[i] != empty)
+			{
+				accessCount++;
+				if (table[i] == v)
+				{
+					accessCount++;
+					return true;
+				}
+				else
+				{
+					i = (i + shift(v, k++) ) % table.Length;
+				}
+			}
+            return false;
+        }
+
+        // wstawianie do tablicy elementu v - zwraca false jesli v juz jest w tablicy
+        // jesli zapelnienie tablicy >= alfa rozmiar tablicy jest podwajany (elementy są przenoszone)
+        public bool Insert(int v)
+        {
+			//Console.WriteLine("Insert");
+
+			int i = hash(v, table.Length);
+			int k = 1;
+			int firstDel = empty;
+			while(table[i] != v && table[i] != empty)
+			{
+				accessCount += 2;
+				if(table[i] == deleted && firstDel == empty)
+				{
+					accessCount++;
+					firstDel = i;
+				}
+				i = (i + shift(v, k++)) % table.Length;
+			}
+			if (table[i] == v)
+			{
+				accessCount++;
+				return false;
+			}
+			if (firstDel != empty)
+			{
+				table[firstDel] = v;
+				return true;
+			}
+			else
+			{
+				elemCount++;
+				table[i] = v;
+			}
+			if (((double)elemCount / table.Length) >= alfa)
+			{
+				int[] pom = new int[table.Length];
+				for (int j = 0; j < table.Length; j++)
+					pom[j] = table[j];
+
+				table = new int[2 * table.Length];
+				for (int j = 0; j < table.Length; j++)
+					table[j] = empty;
+
+				elemCount = 0;
+				for (int j = 0; j < pom.Length; j++)
+				{
+					if (pom[j] != empty && pom[j] != deleted)
+						Insert(pom[j]);
+				}
+			}
+            return true;
+        }
+
+        // usuwanie elementu v z tablicy - zwraca false jesli elementu v nie ma w tablicy
+        public bool Remove(int v)
+		{
+			//Console.WriteLine("Remove");
+
+			if (Search(v) == false)
+				return false;
+
+			//Console.WriteLine("Remove po searchu");
+
+			int i = hash(v, table.Length);
+			int k = 1;
+			while (table[i] != v)
+			{
+				accessCount++;
+				i = (i + shift(v, k++)) % table.Length;
+			}
+			table[i] = deleted;
+			return true;
+        }
+
+        // Funkcje haszujace - dla wszystkich
+        // v     - klucz
+        // size  - rozmiar tablicy
+        // wynik - indeks elementu v w tablicy rozmiaru size
+
+        // Najprostsze haszowanie modulo
+        public static int ModHash(int v, int size)
+        {
+            return v % size;
+        }
+
+        // haszowanie multiplikatywne
+        public static int MultiplyHash(int v, int size)
+        {
+			double a = (Math.Sqrt(5) - 1) / 2;
+			double ulamkowa = v * a - (int)(v * a);
+			int wynik = (int)(size * ulamkowa);
+			// wynik = czesc_calkowita(size*(czesc_ulamkowa(v*a)))
+			return wynik % size; 
+        }
+
+        // Funkcje rozwiazywania kolizji - dla wszystkich
+        // v     - klucz
+        // k     - numer kroku ( przy probie dostepu do klucza v wystapilo juz k kolizji )
+        // wynik - przesuniecie wzgledem aktualnego indeksu
+
+        // sekwencyjne rozwiazywanie kolizji
+        // - szukamy pierwszego wolnego miejsca (z krokiem 1)
+        public static int Shift1(int v, int k) { return 1; }
+
+        // liniowe rozwiazywanie kolizji
+        // - szukamy pierwszego wolnego miejsca z zadanym krokiem > 1 (np. 5)
+        //   (krok musi byc wzglednie pierwszy z rozmiarem tablicy)
+        public static int Shift5(int v, int k) { return 5; }
+
+        // rozwiazywanie kolizji z rosnacym krokiem
+        // - w k-tej probie przesuwamy sie o k
+        public static int Shiftk(int v, int k) { return k; }
+
+        // rozwiazywanie kolizji przy pomocy haszowania dwukrotnego
+        // - rozny krok dla roznych kluczy !
+        // do wyznaczenia kroku zastosowac jakies proste haszowanie modulo
+        // (dla kazdego klucza krok musi byc wzglednie pierwszy z rozmiarem tablicy)
+        public static int Shifth(int v, int k)
+        {
+			return 2 * (v % 1337) + 1;
+        }
+
+    } // class HashTable
+
+} // namespace ASD 
