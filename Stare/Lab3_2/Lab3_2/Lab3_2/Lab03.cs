756fb8cb4cc295198f3ab8243f3d864988be34c1
L-Dogg <szymek94sul@gmail.com>
2016-03-08 20:21:24 +0100

    tests passed

--- Stare/Lab3_2/Lab3_2/Lab3_2/Lab03.cs
+++ Stare/Lab3_2/Lab3_2/Lab3_2/Lab03.cs
@@ -1,30 +1,31 @@
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace ASD.Graphs
 {
 
-public static class BasicGraphExtender
-    {
-    /// <summary>Dodawanie wierzchołka do grafu</summary>
-    /// <param name="g">Graf, do którego dodajemy wierzchołek</param>
-    /// <returns>Graf z dodanym wierzchołkiem</returns>
-    /// <remarks>
-    /// 0.5 pkt.
-    /// Metoda zwraca graf, będący kopią grafu wejściowego z dodanym wierzchołkiem.
-    /// Graf wejściowy pozostaje niezmieniony.
-    /// W utworzonym grafie są takie same krawędzie jak w wejściowym.
-    /// Utworzony graf ma taką samą reprezentację jak graf wejściowy.
-    /// Uwaga: W grafach nieskierowanych nie probować dodawawać po 2 razy tej samej krawędzi
-    /// </remarks>
-    public static Graph AddVertex(this Graph g)
-        {
+	public static class BasicGraphExtender
+	{
+		/// <summary>Dodawanie wierzchołka do grafu</summary>
+		/// <param name="g">Graf, do którego dodajemy wierzchołek</param>
+		/// <returns>Graf z dodanym wierzchołkiem</returns>
+		/// <remarks>
+		/// 0.5 pkt.
+		/// Metoda zwraca graf, będący kopią grafu wejściowego z dodanym wierzchołkiem.
+		/// Graf wejściowy pozostaje niezmieniony.
+		/// W utworzonym grafie są takie same krawędzie jak w wejściowym.
+		/// Utworzony graf ma taką samą reprezentację jak graf wejściowy.
+		/// Uwaga: W grafach nieskierowanych nie probować dodawawać po 2 razy tej samej krawędzi
+		/// </remarks>
+		public static Graph AddVertex(this Graph g)
+		{
 			Graph ret = g.IsolatedVerticesGraph(g.Directed, g.VerticesCount + 1);
 			HashSet<Edge> elist = new HashSet<Edge>();
 			for (int i = 0; i < g.VerticesCount; i++)
 			{
-				foreach(Edge e in g.OutEdges(i))
+				foreach (Edge e in g.OutEdges(i))
 				{
 					if (!elist.Contains(e))
 					{
@@ -32,45 +33,47 @@ public static class BasicGraphExtender
 						elist.Add(e);
 					}
 
-                }
+				}
 			}
 
 			return ret;
-        }
+		}
 
-    /// <summary>Usuwanie wierzchołka z grafu</summary>\
-    /// <param name="g">Graf, z którego usuwamy wierzchołek</param>
-    /// <param name="del">Usuwany wierzchołek</param>
-    /// <returns>Graf z usunietym wierzchołkiem</returns>
-    /// <remarks>
-    /// 1.0 pkt.
-    /// Metoda zwraca graf, będący kopią grafu wejściowego z usuniętym wierzchołkiem.
-    /// Graf wejściowy pozostaje niezmieniony.
-    /// W utworzonym grafie są takie same krawędzie jak w wejściowym
-    /// (oczywiście z wyjątkiem incydentnych z usuniętym wierzchołkiem, numeracja wierzchołków zostaje zaktualizowana)
-    /// Utworzony graf ma taką samą reprezentację jak graf wejściowy.
-    /// Uwaga: W grafach nieskierowanych nie probować dodawawać po 2 razy tej samej krawędzi
-    /// </remarks>
-    public static Graph DeleteVertex(this Graph g, int del)
+		/// <summary>Usuwanie wierzchołka z grafu</summary>\
+		/// <param name="g">Graf, z którego usuwamy wierzchołek</param>
+		/// <param name="del">Usuwany wierzchołek</param>
+		/// <returns>Graf z usunietym wierzchołkiem</returns>
+		/// <remarks>
+		/// 1.0 pkt.
+		/// Metoda zwraca graf, będący kopią grafu wejściowego z usuniętym wierzchołkiem.
+		/// Graf wejściowy pozostaje niezmieniony.
+		/// W utworzonym grafie są takie same krawędzie jak w wejściowym
+		/// (oczywiście z wyjątkiem incydentnych z usuniętym wierzchołkiem, numeracja wierzchołków zostaje zaktualizowana)
+		/// Utworzony graf ma taką samą reprezentację jak graf wejściowy.
+		/// Uwaga: W grafach nieskierowanych nie probować dodawawać po 2 razy tej samej krawędzi
+		/// </remarks>
+		public static Graph DeleteVertex(this Graph g, int del)
 		{
-			Graph a = g.Clone();
-
-			// Usuniecie wszystkich krawedzi incydentnych z wierzcholkiem:
-			foreach(Edge e in a.OutEdges(del))
-			{
-				a.DelEdge(e);
-			}
+			// Czy w ogole istnieje taki wierzcholek:
+			if (del > g.VerticesCount)
+				return g;
 
 			// Utworzenie grafu bez krawedzi na bazie wierzcholkow grafu g bez usuwanego:
-			Graph ret = a.IsolatedVerticesGraph(a.Directed, a.VerticesCount - 1);
+			Graph ret = g.IsolatedVerticesGraph(g.Directed, g.VerticesCount - 1);
 
 			HashSet<Edge> elist = new HashSet<Edge>();
 			// Uzupelnienie wynikowego grafu o krawedzie z uwzglednieniem przesuniecia:
-			for (int i = 0; i < a.VerticesCount; i++)
+			for (int i = 0; i < g.VerticesCount; i++)
 			{
-				foreach(Edge e in a.OutEdges(i))
+				// Omijamy krawedzie z usuwanego wierzcholka:
+				if (i == del)
+					continue;
+				foreach (Edge e in g.OutEdges(i))
 				{
-					if (!elist.Contains(e))
+					// Omijamy krawedzie z usuwanego wierzcholka:
+					if (e.To == del)
+						continue;
+					if (!elist.Contains(new Edge(e.To, e.From, e.Weight)))
 					{
 						if (i < del && e.To < del)
 							ret.AddEdge(i, e.To, e.Weight);
@@ -81,32 +84,32 @@ public static class BasicGraphExtender
 						else
 							ret.AddEdge(i - 1, e.To - 1, e.Weight);
 
-						elist.Add(e);			
+						elist.Add(e);
 					}
 				}
 			}
 			return ret; // zmienic !
-        }
+		}
 
-    /// <summary>Dopełnienie grafu</summary>
-    /// <param name="g">Graf wejściowy</param>
-    /// <returns>Graf będący dopełnieniem grafu wejściowego</returns>
-    /// <remarks>
-    /// 0.5 pkt.
-    /// Dopełnienie grafu to graf o tym samym zbiorze wierzchołków i zbiorze krawędzi równym VxV-E-"pętle"
-    /// Graf wejściowy pozostaje niezmieniony.
-    /// Utworzony graf ma taką samą reprezentację jak graf wejściowy.
-    /// Uwaga 1 : w przypadku stwierdzenia ze graf wejściowy jest grafem ważonym zgłosić wyjątek ArgumentException
-    /// Uwaga 2 : W grafach nieskierowanych nie probować dodawawać po 2 razy tej samej krawędzi
-    /// </remarks>
-    public static Graph Complement(this Graph g)
-        {
+		/// <summary>Dopełnienie grafu</summary>
+		/// <param name="g">Graf wejściowy</param>
+		/// <returns>Graf będący dopełnieniem grafu wejściowego</returns>
+		/// <remarks>
+		/// 0.5 pkt.
+		/// Dopełnienie grafu to graf o tym samym zbiorze wierzchołków i zbiorze krawędzi równym VxV-E-"pętle"
+		/// Graf wejściowy pozostaje niezmieniony.
+		/// Utworzony graf ma taką samą reprezentację jak graf wejściowy.
+		/// Uwaga 1 : w przypadku stwierdzenia ze graf wejściowy jest grafem ważonym zgłosić wyjątek ArgumentException
+		/// Uwaga 2 : W grafach nieskierowanych nie probować dodawawać po 2 razy tej samej krawędzi
+		/// </remarks>
+		public static Graph Complement(this Graph g)
+		{
 			Graph ret = g.IsolatedVerticesGraph();
 
 			HashSet<Edge> elist = new HashSet<Edge>();
 			for (int i = 0; i < g.VerticesCount; i++)
 			{
-				for(int j = 0; j < g.VerticesCount; j++)
+				for (int j = 0; j < g.VerticesCount; j++)
 				{
 					if (i == j)
 						continue;
@@ -114,7 +117,7 @@ public static class BasicGraphExtender
 					if (g.GetEdgeWeight(i, j) == null)
 					{
 						Edge e = new Edge(i, j, 1);
-                        if (!elist.Contains(e))
+						if (!elist.Contains(new Edge(j, i, 1)) && !elist.Contains(e))
 						{
 							ret.AddEdge(i, j);
 							elist.Add(e);
@@ -125,37 +128,43 @@ public static class BasicGraphExtender
 				}
 			}
 			return ret; // zmienic !
-        }
+		}
 
-    /// <summary>Domknięcie grafu</summary>
-    /// <param name="g">Graf wejściowy</param>
-    /// <returns>Graf będący domknięciem grafu wejściowego</returns>
-    /// <remarks>
-    /// 1.5 pkt.
-    /// Domknięcie grafu to graf, w którym krawędzią połączone są wierzchołki, 
-    /// pomiędzy którymi istnieje ścieżka w wyjściowym grafie (pętle wykluczamy).
-    /// Graf wejściowy pozostaje niezmieniony.
-    /// Utworzony graf ma taką samą reprezentację jak graf wejściowy.
-    /// Uwaga 1 : w przypadku stwierdzenia ze graf wejściowy jest grafem ważonym zgłosić wyjątek ArgumentException
-    /// </remarks>
-    public static Graph Closure(this Graph g)
-        {
+		/// <summary>Domknięcie grafu</summary>
+		/// <param name="g">Graf wejściowy</param>
+		/// <returns>Graf będący domknięciem grafu wejściowego</returns>
+		/// <remarks>
+		/// 1.5 pkt.
+		/// Domknięcie grafu to graf, w którym krawędzią połączone są wierzchołki, 
+		/// pomiędzy którymi istnieje ścieżka w wyjściowym grafie (pętle wykluczamy).
+		/// Graf wejściowy pozostaje niezmieniony.
+		/// Utworzony graf ma taką samą reprezentację jak graf wejściowy.
+		/// Uwaga 1 : w przypadku stwierdzenia ze graf wejściowy jest grafem ważonym zgłosić wyjątek ArgumentException
+		/// </remarks>
+		public static Graph Closure(this Graph g)
+		{
 			Graph ret = g.IsolatedVerticesGraph();
 			PathsInfo[] tab = new PathsInfo[g.VerticesCount];
 			HashSet<Edge> elist = new HashSet<Edge>();
 
+			// Dla każdego wierzchołka i znajdź ścieżki do pozostałych wierzchołków:
 			for (int i = 0; i < g.VerticesCount; i++)
 			{
-				if(g.DijkstraShortestPaths(i, out tab))
+				if (g.DijkstraShortestPaths(i, out tab))
 				{
-					for(int j = 0; j < g.VerticesCount; j++)
+					for (int j = 0; j < g.VerticesCount; j++)
 					{
+						// Pomiń pętle:
+						if (i == j)
+							continue;
+
 						if (g.GetEdgeWeight(i, j) > 1)
 							throw new ArgumentException();
 
 						Edge e = new Edge(i, j, 1);
-						if (tab[j].Dist != null && elist.Contains(e) == false)
+						if (tab[j].Dist != null && elist.Contains(new Edge(j, i, 1)) == false && elist.Contains(e) == false)
 						{
+							//Console.WriteLine("Adding edge {0} - {1}", i, j);
 							elist.Add(e);
 							ret.AddEdge(e);
 						}
@@ -164,20 +173,86 @@ public static class BasicGraphExtender
 
 			}
 			return ret; // zmienic !
-        }
+		}
 
-    /// <summary>Badanie czy graf jest dwudzielny</summary>
-    /// <param name="g">Graf wejściowy</param>
-    /// <returns>Informacja czy graf wejściowy jest dwudzielny</returns>
-    /// <remarks>
-    /// 1.5 pkt.
-    /// Graf wejściowy pozostaje niezmieniony.
-    /// </remarks>
-    public static bool IsBipartite(this Graph g)
-        {
-        return false; // zmienic !
-        }
+		/// <summary>Badanie czy graf jest dwudzielny</summary>
+		/// <param name="g">Graf wejściowy</param>
+		/// <returns>Informacja czy graf wejściowy jest dwudzielny</returns>
+		/// <remarks>
+		/// 1.5 pkt.
+		/// Graf wejściowy pozostaje niezmieniony.
+		/// </remarks>
+		 
+		public enum COLORS { NONE, RED, BLUE };
+		public static bool IsBipartite(this Graph g)
+		{
+			COLORS[] col = new COLORS[g.VerticesCount];
 
-    }
+            for (int i = 1; i < g.VerticesCount; i++)
+				col[i] = COLORS.NONE;
+
+			for(int i = 0; i < g.VerticesCount; i++)
+			{
+				if (col[i] == COLORS.NONE)
+				{
+					if (!BFSColoring(g, i, ref col))
+						return false;
+				}
+			}
+
+			return true; // zmienic !
+        }
+		public static bool BFSColoring(Graph g, int v, ref COLORS []col)
+		{
+			Queue<int> q = new Queue<int>();
+			int vert;
+			int partition_class = 0;
+			int i;
+            q.Enqueue(v);
+
+			while(q.Count != 0)
+			{
+				i = 0;
+				vert = q.Dequeue();
+				if (col[vert] == COLORS.NONE || col[vert] == ((partition_class % 2 == 0) ? COLORS.RED : COLORS.BLUE))
+				{
+					if (col[vert] == COLORS.NONE)
+						col[vert] = (partition_class++ % 2 == 0) ? COLORS.RED : COLORS.BLUE;
+					//Console.WriteLine("Partition: {0}", partition_class % 2);
+					//Console.WriteLine("DEQ: Vertex {0} has color {1}", vert, col[vert]);
+					foreach (Edge e in g.OutEdges(vert))
+					{
+						if (col[e.To] == COLORS.NONE)
+						{
+							if (col[vert] == COLORS.RED)
+								col[e.To] = COLORS.BLUE;
+							else
+								col[e.To] = COLORS.RED;
+							//Console.WriteLine("ENQ: Vertex {0} has color {1}", e.To, col[e.To]);
+							q.Enqueue(e.To);
+						}
+						else if (col[e.To] == col[vert])
+						{
+							Console.WriteLine("Vertex {0} (Color {1}) is connected with Vertex {2} (Color {3})", e.To, col[e.To], vert, col[vert]);
+							return false;
+						}
+						else
+							i++;
+					}
+					if (i == g.OutDegree(v))
+						partition_class--;
+				}
+				else
+				{
+					//Console.WriteLine("NIE WESZŁO! Vertex {0} has color {1}", vert, col[vert]);
+				}
+				foreach (Edge e in g.OutEdges(vert))
+					if (col[e.To] == col[vert])
+						return false;
+				partition_class++;
+			}
+			return true;
+		}
+	}
 
 }
--- Stare/Lab3_2/Lab3_2/Lab3_2/Lab03_main.cs
+++ Stare/Lab3_2/Lab3_2/Lab3_2/Lab03_main.cs
@@ -26,12 +26,15 @@ namespace Lab03
                 Console.WriteLine("# licznik: {0}", Graph.Counter);
                 Console.WriteLine("Graf g1 jest typu: {0} i jest skierowany: {1}", g1.GetType(), g1.Directed);
 
+
                 h1 = g1.AddVertex();
                 Console.WriteLine("\nDodawanie\nGraf h1 ma {0} (powinno być 6) wierzchołków, a ostatni wierzchołek ma stopień {1} (powinno być 0)",
                     h1.VerticesCount, h1.InDegree(h1.VerticesCount - 1));
                 Console.WriteLine("Graf h1 ma {0} (powinno być 6) krawędzi", h1.EdgesCount);
                 Console.WriteLine("# licznik: {0}", Graph.Counter);
 
+
+                h1 = g1.DeleteVertex(2);
                 Console.WriteLine("\nUsuwanie\nGraf h1 ma {0} (powinno być 4) wierzchołków, a wierzchołki 1,2,3 mają odpowiednio stopienie {1} (powinno być 1),{2} (powinno być 0),{3} (powinno być 1)",
                     h1.VerticesCount, h1.InDegree(1), h1.InDegree(2), h1.InDegree(3));
                 Console.WriteLine("Graf h1 ma {0} (powinno być 2) krawędzi", h1.EdgesCount);
@@ -42,6 +45,8 @@ namespace Lab03
                 Console.WriteLine("\nDopełnienie g1 ma {0} (powinno być 4) krawędzi", h1.EdgesCount);
                 Console.WriteLine("# licznik: {0}", Graph.Counter);
 
+
+                h1 = g1.Closure();
                 Console.WriteLine("\nDomknięcie g1 ma {0} (powinno być 10) krawędzi", h1.EdgesCount);
                 Console.WriteLine("# licznik: {0}", Graph.Counter);
 
@@ -52,12 +57,14 @@ namespace Lab03
 
                 Console.WriteLine("\nCzy h1 jest dwudzielny ?: {0} (powinno być False)", h1.IsBipartite());
                 Console.WriteLine("# licznik: {0}", Graph.Counter);
+				//new GraphExport().Export(h1, null, "test");
 
-                h1 = rgg.BipariteGraph(typeof(AdjacencyMatrixGraph),30,50,0.5);
+				h1 = rgg.BipariteGraph(typeof(AdjacencyMatrixGraph),30,50,0.5);
                 Console.WriteLine("\nCzy nowy h1 jest dwudzielny ?: {0} (powinno być True)", h1.IsBipartite());
                 Console.WriteLine("# licznik: {0}", Graph.Counter);
+				
 
-                Console.WriteLine("\n\n*************************\n\n");
+				Console.WriteLine("\n\n*************************\n\n");
                 g2.AddEdge(0, 1);
                 g2.AddEdge(2, 1);
                 g2.AddEdge(3, 2);
@@ -90,10 +97,11 @@ namespace Lab03
                 Console.WriteLine("Domknięcie g2 + K1 ma {0} (powinno być 4) krawędzi", h2.EdgesCount);
                 Console.WriteLine("# licznik: {0}", Graph.Counter);
 
-
+				
                 Console.WriteLine("\nCzy h2 jest dwudzielny ?: {0} (powinno być False)", g2.IsBipartite());
                 Console.WriteLine("# licznik: {0}", Graph.Counter);
-            }
+
+			}
             catch(Exception e)
             {
                 Console.WriteLine(e.Message);
