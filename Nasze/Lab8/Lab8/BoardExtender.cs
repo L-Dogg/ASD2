03c2ae24d340b6f3fe36bf3cd733758784d5c466
L-Dogg <szymek94sul@gmail.com>
2016-04-22 10:06:56 +0200

    Finished

--- Nasze/Lab8/Lab8/BoardExtender.cs
+++ Nasze/Lab8/Lab8/BoardExtender.cs
@@ -88,10 +88,67 @@ namespace MyGreedyFish
         /// <returns>Liczba ryb zebrana przez pingwiny</returns>
         public static int GreedyAlgorithm2(this Board board, out Move[] moves)
         {
-            moves = null;
-            int fishes = -1;
-            
-            return fishes; //zwrócona liczba ryb
-        }
+			moves = null;
+			int fishes = 0;
+
+			if (board.penguins == null || board.penguins.Count == 0 || board.AllFishes() == 0)
+				return 0;
+
+			List<Move> myMoves = new List<Move>();
+			while (true)
+			{
+				int maxPenguin = -1;
+				int maxScore = 0;
+				int maxDir = -1;
+				Point bestPoint = new Point(-1, -1);
+
+				for (int i = 0; i < board.penguins.Count; i++)
+				{
+					for (int dir = 0; dir < 6; dir++)
+					{
+						Point dirPoint = board.GetNeighbour(board.penguins[i].x, board.penguins[i].y, dir);
+						while (dirPoint.IsValid() && !board.IsPenguinAtField(dirPoint.x, dirPoint.y) && board.grid[dirPoint.x, dirPoint.y] != 0)
+						{
+							Point pomPoint = board.GetNeighbour(dirPoint.x, dirPoint.y, dir);
+							if(!pomPoint.IsValid() || board.IsPenguinAtField(pomPoint.x, pomPoint.y) || board.grid[pomPoint.x, pomPoint.y] == 0)
+							{
+								if (board.grid[dirPoint.x, dirPoint.y] > maxScore)
+								{
+									maxPenguin = i;
+									maxScore = board.grid[dirPoint.x, dirPoint.y];
+									maxDir = dir;
+									bestPoint = dirPoint;
+								}
+								else if (board.grid[dirPoint.x, dirPoint.y] == maxScore)
+								{
+									if (i > maxPenguin || dir > maxDir)
+									{
+										dirPoint = board.GetNeighbour(dirPoint.x, dirPoint.y, dir);
+										continue;
+									}
+
+									maxPenguin = i;
+									maxScore = board.grid[dirPoint.x, dirPoint.y];
+									maxDir = dir;
+									bestPoint = dirPoint;
+								}
+							}
+							dirPoint = board.GetNeighbour(dirPoint.x, dirPoint.y, dir);
+						}
+					}
+				}
+				if (maxPenguin == -1 || maxScore == 0)
+					break;
+				int x = board.penguins[maxPenguin].x;
+				int y = board.penguins[maxPenguin].y;
+				Move makeMove = new Move(new Point(x, y), bestPoint, board.grid[x, y], maxPenguin);
+				myMoves.Add(makeMove);
+				fishes += board.MovePenguin(maxPenguin, bestPoint.x, bestPoint.y);
+				board.grid[x, y] = 0;
+
+			}
+			moves = myMoves.ToArray();
+			return fishes; //zwrócona liczba ryb
+		}
     }
 }
